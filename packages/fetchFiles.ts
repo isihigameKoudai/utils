8b6e9e93cb6361b5f045ceeaa14a44aa6f3abdf4
koudaiishigame <kira5052@gmail.com>
2021-12-18 19:04:29 +0900

    feat: first commit

---
+++ packages/fetchFiles.ts
@@ -0,0 +1,113 @@
+type ImageDetail = ".jpeg" | ".jpg" | ".png" | ".gif";
+type MovieDetail = ".mp4" | ".avi" | ".fiv" | ".mov" | ".wmv";
+type AudioDetail =
+  | ".wav"
+  | ".mp3"
+  | ".wma"
+  | ".aac"
+  | ".m4a"
+  | ".flac"
+  | ".ogg";
+const imageDetails: ImageDetail[] = [".jpeg", ".jpg", ".png", ".gif"];
+const movieDetails: MovieDetail[] = [".mp4", ".avi", ".fiv", ".mov", ".wmv"];
+const audioDetails: AudioDetail[] = [
+  ".wav",
+  ".mp3",
+  ".wma",
+  ".aac",
+  ".m4a",
+  ".flac",
+  ".ogg",
+];
+
+const IMAGE_TYPE = "image" as const;
+const MOVIE_TYPE = "movie" as const;
+const AUDIO_TYPE = "audio" as const;
+
+const detailsBy = {
+  [IMAGE_TYPE]: imageDetails,
+  [AUDIO_TYPE]: audioDetails,
+  [MOVIE_TYPE]: movieDetails,
+};
+
+type Option = {
+  isMultiple?: boolean;
+  fileType?: typeof IMAGE_TYPE | typeof MOVIE_TYPE | typeof AUDIO_TYPE;
+  details?: ImageDetail[] | AudioDetail[] | MovieDetail[];
+};
+
+type FetchFiles = (option?: Option) => Promise<{
+  status: string;
+  files: File[];
+}>;
+
+const initialOption: Option = {
+  isMultiple: false,
+};
+
+export const fetchFiles: FetchFiles = ({
+  isMultiple = false,
+  fileType,
+  details,
+} = initialOption) => {
+  return new Promise((resolve, reject) => {
+    const isAvailable: boolean = !!(
+      window.File &&
+      window.FileReader &&
+      window.FileList &&
+      window.Blob
+    );
+    if (!isAvailable) {
+      reject({
+        status: "The File APIs are not fully supported in this browser.",
+        files: [],
+      });
+      return;
+    }
+
+    const $input: HTMLInputElement = document.createElement("input");
+    $input.type = "file";
+    $input.multiple = isMultiple;
+    if (fileType && !details) {
+      $input.accept = `${fileType}/*`;
+      // TODO: detailsがある時ファイル拡張子を細かく指定できるようにする
+    }
+    $input.onchange = (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      if (!target || !target.files) {
+        reject({
+          status: "No Files",
+          files: [],
+        });
+        return;
+      }
+
+      const files = [...target.files];
+      resolve({
+        status: "succeed",
+        files,
+      });
+    };
+    $input.click();
+  });
+};
+
+export const fetchImage: FetchFiles = () => {
+  return fetchFiles({ fileType: "image", isMultiple: false });
+};
+
+export const fetchImages: FetchFiles = () => {
+  return fetchFiles({ fileType: "image", isMultiple: true });
+};
+
+export const fetchAudio: FetchFiles = () =>
+  fetchFiles({ fileType: "audio", isMultiple: false });
+
+export const fetchAudios: FetchFiles = () =>
+  fetchFiles({ fileType: "audio", isMultiple: true });
+
+export const fetchMovie: FetchFiles = () =>
+  fetchFiles({ fileType: "audio", isMultiple: false });
+
+export const fetchMovies: FetchFiles = () =>
+  fetchFiles({ fileType: "audio", isMultiple: true });
---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useState, useCallback } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { fetchAudios, fetchFiles, fetchImages, fetchMovies } from '../packages/fetchFiles'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const onOpenFile = useCallback(async () => {
+    const files = await fetchFiles()
+    console.log(files);
+  },[])
+
+  const onOpenImages = useCallback(async () => {
+    const { files } = await fetchImages();
+    console.log('orpn images',files);
+  },[]);
+
+  const onOpenAudios = useCallback(async () => {
+    const { files } = await fetchAudios()
+    console.log('open audios', files);
+  },[]);
+
+  const onOpenMovies = useCallback(async () => {
+    const { files } = await fetchMovies()
+    console.log('open movies', files);
+  },[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={onOpenFile}>
+            open files
+          </button>
+          <button type="button" onClick={onOpenImages}>
+            image files
+          </button>
+          <button type="button" onClick={onOpenMovies}>
+            video files
+          </button>
+          <button type="button" onClick={onOpenAudios}>
+            audio files
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
