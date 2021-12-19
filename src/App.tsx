96605a867cacf4b2247a50148db0176fbe5fe760
koudaiishigame <kira5052@gmail.com>
2021-12-19 14:20:41 +0900

    add env

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import { useState, useCallback } from 'react'
 import './App.css'
 
-import { fetchAudios, fetchFiles, fetchImages, fetchMovies } from '../packages/fetchFiles'
+import { fetchImage } from '../packages/fetchFiles'
 
 function App() {
   const [count, setCount] = useState(0)
-
   const onOpenFile = useCallback(async () => {
-    const files = await fetchFiles()
-    console.log(files);
+    // const { files } = await fetchImage()
+    // console.log(files);
+    try {
+      const res = await fetch(`https://customsearch.googleapis.com/customsearch/v1?key=${import.meta.env.VITE_GOOGLE_API_KEY}`)
+    console.log(res);
+    } catch(e) {
+      console.error(e);
+    }
   },[])
-
-  const onOpenImages = useCallback(async () => {
-    const { files } = await fetchImages();
-    console.log('orpn images',files);
-  },[]);
-
-  const onOpenAudios = useCallback(async () => {
-    const { files } = await fetchAudios()
-    console.log('open audios', files);
-  },[]);
-
-  const onOpenMovies = useCallback(async () => {
-    const { files } = await fetchMovies()
-    console.log('open movies', files);
-  },[]);
 
   return (
     <div className="App">
@@ -34,37 +24,6 @@ function App() {
           <button type="button" onClick={onOpenFile}>
             open files
           </button>
-          <button type="button" onClick={onOpenImages}>
-            image files
-          </button>
-          <button type="button" onClick={onOpenMovies}>
-            video files
-          </button>
-          <button type="button" onClick={onOpenAudios}>
-            audio files
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
