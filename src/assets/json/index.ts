0f6529fe9055027e021d7df9977e9f238209063c
koudaiishigame <kira5052@gmail.com>
2022-02-21 23:22:56 +0900

    add december

--- src/assets/json/index.ts
+++ src/assets/json/index.ts
@@ -2,6 +2,7 @@ import { DailySales } from "../../model/DailySales";
 
 import csv2021 from "./2021.json";
 
+// 日別売り上げ
 export const dailySalesList: DailySales[] = csv2021.map(
   (item) => new DailySales(item)
 );
