f7c7b23611ca4fd61445023f5c73ad2db7cd9922
koudaiishigame <kira5052@gmail.com>
2022-06-02 23:59:44 +0900

    add audio

---
+++ packages/audio.ts
@@ -0,0 +1,62 @@
+/**
+ * 音声データの再生、停止、一時停止、再開を行うクラス
+ */
+class Audio {
+  context: AudioContext;
+  audioSource: AudioBufferSourceNode;
+  isPlaying: boolean;
+
+  constructor() {
+    this.context =
+      new AudioContext() ||
+      new (window.AudioContext || window.webkitAudioContext)();
+    this.audioSource = this.context.createBufferSource();
+    this.isPlaying = false;
+  }
+
+  /**
+   * 音声データの登録
+   * @param arrayBuffer
+   */
+  async setAudio(arrayBuffer: ArrayBuffer) {
+    this.audioSource = this.context.createBufferSource();
+    const audioBuffer = await this.context.decodeAudioData(arrayBuffer);
+    this.audioSource.buffer = audioBuffer;
+  }
+
+  /**
+   * 再生（登録されている音声を再生）
+   */
+  play() {
+    if (this.audioSource) {
+      this.audioSource.disconnect();
+    }
+
+    this.audioSource.connect(this.context.destination);
+    this.audioSource.start(0);
+    this.isPlaying = true;
+  }
+
+  /**
+   * 一時停止/再生
+   */
+  pause() {
+    if (this.isPlaying) {
+      this.context.suspend();
+      this.isPlaying = false;
+    } else {
+      this.context.resume();
+      this.isPlaying = true;
+    }
+  }
+
+  /**
+   * 停止
+   */
+  stop() {
+    this.audioSource.stop();
+    this.isPlaying = false;
+  }
+}
+
+export default new Audio();
