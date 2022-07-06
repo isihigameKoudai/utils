af1d33f876647608f0d21c894a82894472beaee5
koudaiishigame <kira5052@gmail.com>
2022-07-06 23:35:20 +0900

    fix adudio

--- packages/audio.ts
+++ packages/audio.ts
@@ -4,16 +4,24 @@
  * https://developer.mozilla.org/ja/docs/Web/API/Web_Audio_API
  */
 export default class Audio {
-  context: AudioContext;
-  audioSource: AudioBufferSourceNode;
+  _context: AudioContext;
+  _audioSource: AudioBufferSourceNode;
   isPlaying: boolean;
 
   constructor() {
-    this.context =
+    this._context =
       new AudioContext() ||
       new (window.AudioContext || window.webkitAudioContext)();
-    this.audioSource = this.context.createBufferSource();
+    this._audioSource = this._context.createBufferSource();
     this.isPlaying = false;
+  }
+
+  get context(): AudioContext {
+    return this._context;
+  }
+
+  get audioSource(): AudioBufferSourceNode {
+    return this._audioSource;
   }
 
   /**
@@ -21,21 +29,21 @@ export default class Audio {
    * @param arrayBuffer
    */
   async setAudio(arrayBuffer: ArrayBuffer) {
-    this.audioSource = this.context.createBufferSource();
-    const audioBuffer = await this.context.decodeAudioData(arrayBuffer);
-    this.audioSource.buffer = audioBuffer;
+    this._audioSource = this._context.createBufferSource();
+    const audioBuffer = await this._context.decodeAudioData(arrayBuffer);
+    this._audioSource.buffer = audioBuffer;
   }
 
   /**
    * 再生（登録されている音声を再生）
    */
   play() {
-    if (this.audioSource) {
-      this.audioSource.disconnect();
+    if (this._audioSource) {
+      this._audioSource.disconnect();
     }
 
-    this.audioSource.connect(this.context.destination);
-    this.audioSource.start(0);
+    this._audioSource.connect(this._context.destination);
+    this._audioSource.start(0);
     this.isPlaying = true;
   }
 
@@ -44,10 +52,10 @@ export default class Audio {
    */
   pause() {
     if (this.isPlaying) {
-      this.context.suspend();
+      this._context.suspend();
       this.isPlaying = false;
     } else {
-      this.context.resume();
+      this._context.resume();
       this.isPlaying = true;
     }
   }
@@ -56,9 +64,9 @@ export default class Audio {
    * 停止
    */
   stop() {
-    this.audioSource.stop();
-    this.audioSource.disconnect();
-    this.audioSource.buffer = null;
+    this._audioSource.stop();
+    this._audioSource.disconnect();
+    this._audioSource.buffer = null;
     this.isPlaying = false;
   }
 }
