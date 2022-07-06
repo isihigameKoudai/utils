ead5f72c2e0292fc93b9e90bac9e3bfaf5b740e3
koudaiishigame <kira5052@gmail.com>
2022-07-07 00:17:20 +0900

    add june sales

--- src/assets/json/csv2022/index.ts
+++ src/assets/json/csv2022/index.ts
@@ -3,5 +3,6 @@ import csv02 from "./02.json";
 import csv03 from "./03.json";
 import csv04 from "./04.json";
 import csv05 from "./05.json";
+import csv06 from "./06.json";
 
-export default [...csv01, ...csv02, ...csv03, ...csv04, ...csv05];
+export default [...csv01, ...csv02, ...csv03, ...csv04, ...csv05, ...csv06];
