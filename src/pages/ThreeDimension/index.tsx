80e7f5a262b3b48c523b2c6e264185f1be4f7e95
koudaiishigame <kira5052@gmail.com>
2022-11-23 23:33:20 +0900

    add boxes

--- src/pages/ThreeDimension/index.tsx
+++ src/pages/ThreeDimension/index.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+
+import { Canvas, MeshProps, useFrame } from '@react-three/fiber';
+
+const Box = (props: MeshProps) => {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, setHovered] = useState<boolean>(false);
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  useFrame((state, delta) => {
+    return (ref.current.rotation.x += 0.01)
+  });
+
+  const handleClick = useCallback(() => {
+    setClicked(!clicked);
+  },[clicked]);
+
+  return (
+    <mesh {...props} ref={ref} scale={clicked ? 2 : 1} onClick={handleClick} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}>
+      <boxGeometry args={[1,1,1]} />
+      <meshStandardMaterial color={hovered ? '#ff0000' : '#0000ff'} />
+    </mesh>
+  );
+}
 
 const ThreeDimension: React.FC = () => {
-  return <div>ThreeFiber</div>;
+  return <div id="3D" style={{
+    width: '100%',
+    height: '100vh'
+  }}>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[10,10, 0]} />
+      <Box position={[-1, 0, 0]} />
+      <Box position={[1, 0, 0]}/>
+    </Canvas>
+  </div>;
 }
 
 export default ThreeDimension;
