6c9b8c0987dd03d970c3a5eea1cbe6b6e938704e
koudaiishigame <kira5052@gmail.com>
2023-12-10 17:48:03 +0900

    mouse circle

--- src/pages/Examples/NormalCircle/index.tsx
+++ src/pages/Examples/NormalCircle/index.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as THREE from 'three';
 
 import ShaderCanvas from '../../../../packages/ShaderCanvas';
 import vertex from '../../../../packages/glsl/vertex.vert?raw';
 import normalCircle from './normalCircle.frag?raw';
 
+// {
+//   if (this.timer) clearTimeout(this.timer);
+//   this.coords.set((x / Common.width!) * 2 - 1, -(y / Common.height!) * 2 + 1);
+//   this.mouseMoved = true;
+//   this.timer = setTimeout(() => {
+//     this.mouseMoved = false;
+//   }, 100);
+// }
+
 const NormalCirclePage: React.FC = () => {
+  
+  const uniforms = {
+    time: {
+      value: 0
+    },
+    resolution: {
+      value: new THREE.Vector2(window.innerWidth, window.innerHeight)
+    },
+    x: {
+      value: window.innerWidth
+    },
+    y: {
+      value: window.innerHeight
+    },
+  };
+
+  const follower = (x: number, y: number) => {
+
+  }
+
+  useEffect(() => {
+    // document.addEventListener('mousemove', (e) => {
+    //   console.log(e.pageY, e.clientY, e.screenY)
+    //   uniforms.x.value = e.clientX;
+    //   uniforms.y.value = e.screenY;
+    // });
+    document.addEventListener('mousemove', (e) => {
+
+    
+      uniforms.x.value = e.clientX;
+      uniforms.y.value = e.screenY;
+
+      // this.mouseMoved = true;
+    });
+  },[]);
   
   return <div id="3D" style={{
     width: '100%',
     height: '100svh'
   }}>
     <ShaderCanvas
-      uniforms={{
-        time: {
-          value: 0
-        },
-        resolution: {
-          value: new THREE.Vector2(window.innerWidth, window.innerHeight)
-        }
-      }}
+      uniforms={uniforms}
       vertexShader={vertex}
       fragmentShader={normalCircle}
     />
--- src/pages/Examples/NormalCircle/normalCircle.frag
+++ src/pages/Examples/NormalCircle/normalCircle.frag
@@ -2,8 +2,17 @@ precision mediump float;
 uniform float time;
 uniform vec2 resolution;
 
+uniform float x;
+uniform float y;
+
+float times = 2.0;
+
 void main(void){
-  vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+  // 中心（normal）
+  // vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+  // x,y指定
+  vec2 p = (gl_FragCoord.xy * times - vec2(x * times, y * -times)) / min(resolution.x, resolution.y);
+  // float f = length(p);
   float f = 0.01 / abs(length(p) - 0.5);
   gl_FragColor = vec4(vec3(f), 1.0);
 }
