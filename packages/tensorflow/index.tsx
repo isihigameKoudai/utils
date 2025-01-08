7517c1b16e8ef3154371005b7d97fdae1c279917
koudaiishigame <kira5052@gmail.com>
2025-01-08 22:37:45 +0900

    add PoseDetection

--- packages/tensorflow/index.tsx
+++ packages/tensorflow/index.tsx
@@ -2,3 +2,4 @@ export { VisualDetection, type DetectedObject } from "./VisualDetection";
 export { FaceDetection } from "./FaceDetection";
 export { HandPoseDetection } from "./HandPoseDetection";
 export { FaceLandmarkDetection } from "./FaceLandmarkDetection";
+export { PoseDetection, type Pose } from "./PoseDetection";
