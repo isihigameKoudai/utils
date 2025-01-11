0db3fcd30c4de23d417d5c6fc260cc88c33abd98
koudaiishigame <kira5052@gmail.com>
2025-01-11 17:51:02 +0900

    fix type

--- packages/i-state/type.ts
+++ packages/i-state/type.ts
@@ -2,7 +2,7 @@ export type State = Record<string, any>;
 
 export type Queries<S extends State> = Record<string, (state: S) => any>;
 
-type ActionContext<S extends State, Q extends Queries<S>> = {
+export type ActionContext<S extends State, Q extends Queries<S>> = {
   state: S;
   queries: { [K in keyof Q]: ReturnType<Q[K]> };
   dispatch: <K extends keyof S>(key: K, value: S[K]) => void;
@@ -10,13 +10,13 @@ type ActionContext<S extends State, Q extends Queries<S>> = {
 
 export type Actions<S extends State, Q extends Queries<S>> = Record<
   string,
-  ((context: ActionContext<S, Q>) => void) | ((context: ActionContext<S, Q>, ...args: any[]) => void)
+  ((context: ActionContext<S, Q>, ...args: any[]) => void | Promise<void>)
 >;
 
 export type Store<S extends State, Q extends Queries<S>, A extends Actions<S, Q>> = {
   useStore: () => {
     state: S;
     queries: { [K in keyof Q]: ReturnType<Q[K]> };
-    actions: { [K in keyof A]: A[K] extends (context: any, ...args: infer P) => void ? (...args: P) => void : () => void };
+    actions: { [K in keyof A]: A[K] extends (context: ActionContext<S,Q>, ...args: infer P) => void ? (...args: P) => void : () => void };
   };
 };
