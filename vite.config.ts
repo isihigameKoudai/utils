406ab763cb95aff2467c7c4a820453a167f10184
koudaiishigame <kira5052@gmail.com>
2025-01-12 21:42:35 +0900

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import Pages from "vite-plugin-pages";
 
@@ -6,10 +7,8 @@ import Pages from "vite-plugin-pages";
 export default defineConfig({
   plugins: [react(), Pages()],
   resolve: {
-    alias: [{ find: "@/", replacement: "./src/" }]
-
-    // alias: {
-    //   '@': './'
-    // },
+    alias: {
+      '@': path.resolve(__dirname, './')
+    }
   },
 });
