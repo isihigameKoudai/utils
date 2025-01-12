f3cee5468ea539823b1a270145f6b294282089f5
koudaiishigame <kira5052@gmail.com>
2025-01-12 15:36:50 +0900

    create defineStore

---
+++ src/pages/CryptoCharts/store/crypto.ts
@@ -0,0 +1,29 @@
+import { defineStore } from "../../../../packages/i-state";
+import { Trade } from "../model/CandleStick";
+import { fetchTradeDataList, CryptoListParams } from "../api/crypto";
+import { CandleStick } from "../model/CandleStick";
+
+interface CryptoState {
+  trades: Trade[];
+}
+
+const initialState: CryptoState = {
+  trades: [],
+};
+
+export const CryptoStore = defineStore({
+  state: initialState,
+  queries:{
+    candleSticks: (state) => state.trades.map(trade => new CandleStick(trade)),
+  },
+  actions: {
+    async fetchTrades({ dispatch }, payload: CryptoListParams) {
+      try {
+        const trades = await fetchTradeDataList(payload);
+        dispatch('trades', trades);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  }
+});
