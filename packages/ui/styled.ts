2300c51bf160f67f63f6708f7372c14fe457e234
koudaiishigame <kira5052@gmail.com>
2025-01-13 22:59:53 +0900

    add styledZero

--- packages/ui/styled.ts
+++ packages/ui/styled.ts
@@ -1,4 +1,4 @@
-import { forwardRef, ComponentPropsWithRef, createElement, DetailedHTMLProps } from 'react';
+import React, { forwardRef, ComponentPropsWithRef, createElement, DetailedHTMLProps } from 'react';
 import { style as _style } from 'typestyle';
 
 import { Theme, useTheme } from './theme';
@@ -16,6 +16,29 @@ export function styled<T extends keyof JSX.IntrinsicElements>(Component: T) {
         const { theme } = useTheme();
         const { className: parentClassName, ...rest } = props;
         const localClassName = _style(themeFunction(theme));
+        
+        const className = parentClassName
+          ? `${localClassName} ${parentClassName}`
+          : localClassName;
+
+        return createElement(Component, {
+          ...rest,
+          ref,
+          className
+        });
+      }
+    );
+  };
+}
+
+export function styledZero<T extends keyof JSX.IntrinsicElements>(Component: T) {
+  return (currentStyle: React.CSSProperties | (() => React.CSSProperties)) => {
+    type Props = ComponentPropsWithRef<T> & StyledOptions;
+    
+    return forwardRef<JSX.IntrinsicElements[T] extends DetailedHTMLProps<any, infer E> ? E : never, Props>(
+      (props, ref) => {
+        const { className: parentClassName, ...rest } = props;
+        const localClassName = _style(currentStyle instanceof Function ? currentStyle() : currentStyle);
         
         const className = parentClassName
           ? `${localClassName} ${parentClassName}`
