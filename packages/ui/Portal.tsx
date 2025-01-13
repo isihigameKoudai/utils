e5218054df50b5766156ab6c628d3e11ba019430
koudaiishigame <kira5052@gmail.com>
2025-01-13 23:27:25 +0900

    add Portal

---
+++ packages/ui/Portal.tsx
@@ -0,0 +1,18 @@
+import{ createPortal } from 'react-dom';
+
+interface Props {
+  children: React.ReactNode;
+  id: string;
+}
+
+const Portal: React.FC<Props> = ({ children, id }) => {
+  const targetEl = document.getElementById(id);
+
+  if (!targetEl) {
+    return null;
+  }
+
+  return createPortal(children, targetEl);
+};
+
+export default Portal;
