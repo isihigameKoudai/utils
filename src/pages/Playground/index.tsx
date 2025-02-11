bf6f3dce8a5266885f937b95d89f58b103b37577
koudaiishigame <kira5052@gmail.com>
2025-02-11 18:04:07 +0900

    fixerro

--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -1,21 +1,19 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 
-import { styled } from '../../../packages/ui/styled';
+import { styled } from '@/packages/ui/styled';
 
-const Button = styled('button')((theme) => ({
-  backgroundColor: theme.palette.primary.main,
-  color: theme.palette.primary.contrastText,
+const Button = styled('button')({
   padding: '10px 20px',
   borderRadius: '5px'
-}));
+});
 
 const PlaygroundPage: React.FC = () => {
   const [open, setOpen] = useState(false);
-  const customItems = [...Array(100)].map((_, i) => ({
-    id: i,
-    name: `Item ${i}`,
-    height: Math.floor(Math.random() * 100) + 100
-  }));
+  // const customItems = [...Array(100)].map((_, i) => ({
+  //   id: i,
+  //   name: `Item ${i}`,
+  //   height: Math.floor(Math.random() * 100) + 100
+  // }));
 
   return (
     <div>
