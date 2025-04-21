2ce7a72ad779ad03d7ca90b294a87dd5ff195870
koudaiishigame <kira5052@gmail.com>
2025-04-21 22:43:48 +0900

    fix styled

--- utils/ui/styled.ts
+++ utils/ui/styled.ts
@@ -1,5 +1,5 @@
 import React, { forwardRef, ComponentPropsWithRef, createElement, DetailedHTMLProps } from 'react';
-import { style as _style } from 'typestyle';
+import { style as _style, types } from 'typestyle';
 
 type StyledOptions = {
   className?: string;
@@ -17,7 +17,7 @@ type StyledOptions = {
  * })
  */
 export function styled<T extends keyof JSX.IntrinsicElements>(Component: T) {
-  return (style: React.CSSProperties) => {
+  return (style: types.NestedCSSProperties) => {
     type Props = ComponentPropsWithRef<T> & StyledOptions;
     
     return forwardRef<JSX.IntrinsicElements[T] extends DetailedHTMLProps<any, infer E> ? E : never, Props>(
