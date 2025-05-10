0efc126bf9fcbe396f84f5095dafb7e81ddc6a65
koudaiishigame <kira5052@gmail.com>
2025-05-10 21:19:22 +0900

    add async

--- utils/file.ts
+++ utils/file.ts
@@ -1,3 +1,31 @@
+import fs from 'fs';
+import { promisify } from 'util';
+
+// ファイル読み書き系
+export const readFileAsync = promisify(fs.readFile);
+export const writeFileAsync = promisify(fs.writeFile);
+export const appendFileAsync = promisify(fs.appendFile);
+export const copyFileAsync = promisify(fs.copyFile);
+export const truncateAsync = promisify(fs.truncate);
+export const unlinkAsync = promisify(fs.unlink);
+export const rmAsync = promisify(fs.rm);
+
+// ディレクトリ操作系
+export const mkdirAsync = promisify(fs.mkdir);
+export const rmdirAsync = promisify(fs.rmdir);
+export const readdirAsync = promisify(fs.readdir);
+export const opendirAsync = promisify(fs.opendir);
+
+// ファイルパス系
+export const statAsync = promisify(fs.stat);
+export const existsAsync = promisify(fs.exists);
+export const lstatAsync = promisify(fs.lstat);
+export const readlinkAsync = promisify(fs.readlink);
+export const realpathAsync = promisify(fs.realpath);
+export const symlinkAsync = promisify(fs.symlink);
+export const chmodAsync = promisify(fs.chmod);
+export const chownAsync = promisify(fs.chown);
+
 /**
  * CSV形式のテキストを2次元配列に変換する
  * ex: csv2array('a,b,c\nd,e,f\n') => [['a','b','c'],['d','e','f']]
