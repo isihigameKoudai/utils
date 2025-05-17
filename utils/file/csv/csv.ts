cebc2ccdd1262c926ef3a6fb94e6a87daf5eac61
koudaiishigame <kira5052@gmail.com>
2025-05-17 22:54:04 +0900

    add csv

---
+++ utils/file/csv/csv.ts
@@ -0,0 +1,122 @@
+export class CSV<C extends string> {
+  /**
+   * 元となるCSVデータ
+   * 
+   * @example
+   * const csv = [
+   *  ['header1', 'header2', 'header3'],
+   *  ['value1-1', 'value1-2', 'value1-3'],
+   *  ['value2-1', 'value2-2', 'value2-3'],
+   * ];
+   */
+  readonly _value: string[][];
+
+  /**
+   * 
+   * Mapを使ったCSVデータ
+   * 
+   * @example
+   * const csv = [
+   *  new Map<string, string>([['header1', 'value1-1'], ['header2', 'value1-2'], ['header3', 'value1-3']]),
+   *  new Map<string, string>([['header1', 'value2-1'], ['header2', 'value2-2'], ['header3', 'value2-3']]),
+   * ];
+   */
+  private _mapList: Map<C, string>[];
+
+  constructor(originCsv: string[][]) {
+    this.validate(originCsv);
+
+    this._value = originCsv;
+    this._mapList = this.createMap(originCsv);
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get headers() {
+    return this._value[0];
+  }
+
+  /**
+   * CSVデータに該当しない場合エラーにする
+   * 
+   * ・空の場合
+   * ・カラムの数が一致しない場合
+   * ・ヘッダーに値する文字列が存在しない場合
+   */
+  validate(csv: string[][]) {
+    // 空の場合
+    if (csv.length === 0) {
+      throw new Error('CSV is empty');
+    } 
+
+    // カラムの数が一致しない場合
+    const isDiffColumnCount = csv.some((row) => row.length !== csv[0].length);
+    if (isDiffColumnCount) {
+      throw new Error('The number of columns does not match');
+    }
+  }
+
+  createMap(csv: string[][]): Map<C, string>[] {
+    return csv.map((row) => {
+      return new Map<C, string>(row.map((value, index) => [this.headers[index] as C, value]));
+    });
+  }
+
+  /**
+   * @example
+   * const csv = new Map<{'header1' | 'header2' | 'header3'}>([
+   *  ['header1', 'header2', 'header3'],
+   *  ['value1-1', 'value1-2', 'value1-3'],
+   *  ['value2-1', 'value2-2', 'value2-3'],
+   * ]);
+   * 
+   * const record = csv.getRecord('header1', 'value1-1');
+   * => ['value1-1', 'value1-2', 'value1-3']
+   */
+  getRecords(key: C, where: string) {
+    const targets = this._mapList.filter((map) => map.get(key) === where);
+    if (targets.length === 0) {
+      throw new Error('Record not found');
+    }
+    return targets.map((map) => map.get(key) || '');
+  }
+
+  /**
+   * @example
+   * const csv = new Map<{'header1' | 'header2' | 'header3'}>([
+   *  ['header1', 'header2', 'header3'],
+   *  ['value1-1', 'value1-2', 'value1-3'],
+   *  ['value2-1', 'value2-2', 'value2-3'],
+   * ]);
+   * 
+   * const columns = csv.getColumn('header1');
+   * => ['value1-1', 'value2-1']
+   */
+  getColumn(key: C): string[] {
+    if (!this._mapList.some((map) => map.has(key))) {
+      throw new Error('Column not found');
+    }
+
+    return this._mapList.map((map) => map.get(key) || '');
+  }
+
+  /**
+   * @example
+   * const csv = new Map<{'header1' | 'header2' | 'header3'}>([
+   *  ['header1', 'header2', 'header3'],
+   *  ['value1-1', 'value1-2', 'value1-3'],
+   *  ['value2-1', 'value2-2', 'value2-3'],
+   * ]);
+   * 
+   * const columns = csv.getColumns(['header2', 'header3']);
+   * => [
+   *  ['value1-2', 'value1-3'],
+   *  ['value2-2', 'value2-3'],
+   * ]
+   */
+  getColumns(keys: C[]): string[][] {
+    return keys.map((key) => this.getColumn(key));
+  }
+}
